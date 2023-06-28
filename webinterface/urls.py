1401dc795db26cb59b6ad1375a8a4dbe930942f9
s362692 <nico.hasler@stud-mail.uni-wuerzburg.de>
2023-06-28 09:01:15 +0200

    Added Download CSV Results

--- webinterface/urls.py
+++ webinterface/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
 from webinterface import views
 
 app_name = 'webinterface'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('download-csv/', views.download_csv, name='download_csv'),
 ]
+
