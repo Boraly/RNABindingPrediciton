b3b4b46b4bed61714445a240f54d450b9c8871ce
s362692 <nico.hasler@stud-mail.uni-wuerzburg.de>
2023-06-28 08:50:37 +0200

    Better lineup

--- webinterface/forms.py
+++ webinterface/forms.py
@@ -1,13 +1,8 @@
 from django import forms
 
 
-class PredictionForm(forms.Form):
+class TextInputForm(forms.Form):
     text_input = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 4, 'cols': 50}),
-        label='Text eingeben:'
-    )
-    file_input = forms.FileField(label='Datei hochladen:')
-    predictor_select = forms.ChoiceField(
-        choices=[('species1', 'Species 1 Predictor'), ('species2', 'Species 2 Predictor')],
-        label='Predictor auswählen:'
+        label='Copy and paste the text here.',
+        widget=forms.Textarea
     )
